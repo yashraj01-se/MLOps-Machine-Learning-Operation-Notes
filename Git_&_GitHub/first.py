8bc6dcc7d51a12ff23d2cd9672cf52615461718a
Yashraj Sharma <yashrajsharma12012002@gmail.com>
2026-01-05 12:19:01 +0530

    First-Commit

---
+++ Git_&_GitHub/first.py
@@ -0,0 +1,5 @@
+##Basic Py code
+print("Hello, Git & GitHub!")
+print("This is my first file in a GitHub repository.")
+print("I'm excited to learn more about version control and collaboration!")
+print("Let's make some changes and track them with Git.")
