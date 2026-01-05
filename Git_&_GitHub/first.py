240136dad8f9a87640ae2e158e7e4f1924d2627c
Yashraj Sharma <yashrajsharma12012002@gmail.com>
2026-01-05 12:21:48 +0530

    Second-Commit

--- Git_&_GitHub/first.py
+++ Git_&_GitHub/first.py
@@ -1,5 +1,6 @@
 ##Basic Py code
 print("Hello, Git & GitHub!")
-print("This is my first file in a GitHub repository.")
 print("I'm excited to learn more about version control and collaboration!")
 print("Let's make some changes and track them with Git.")
+
+print("This is my second attempt to GitHub!")
