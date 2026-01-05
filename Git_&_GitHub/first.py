317a1a04b67b1fa66cd314fbe9f8bd5a480cbb5a
Yashraj Sharma <yashrajsharma12012002@gmail.com>
2026-01-05 12:46:10 +0530

    Fourth-Commit

--- Git_&_GitHub/first.py
+++ Git_&_GitHub/first.py
@@ -2,5 +2,4 @@
 print("Hello, Git & GitHub!")
 print("I'm excited to learn more about version control and collaboration!")
 print("Let's make some changes and track them with Git.")
-
 print("This is my second attempt to GitHub!")
