6419639bdda5e97c58cb583962da21aeb85ec62d
Yashraj Sharma <yashrajsharma12012002@gmail.com>
2026-01-06 12:45:08 +0530

    DVC Practice

---
+++ DVC/Data_Versioning_Practice/Code.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os
+
+# Create a simple DataFrame with column names
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie'],
+    'Age': [25, 30, 35],
+    'City': ['New York', 'Los Angeles', 'Chicago']
+}
+
+df = pd.DataFrame(data)
+
+# Adding new row to df for V2
+# new_row_loc = {'Name': 'D1', 'Age': 30, 'City': 'City1'}
+# df.loc[len(df.index)] = new_row_loc
+
+# Adding new row to df for V3
+# new_row_loc = {'Name': 'V1', 'Age': 32, 'City': 'City1'}
+# df.loc[len(df.index)] = new_row_loc
+
+# Ensure the 'data' directory exists at the root level
+data_dir = 'data'
+os.makedirs(data_dir, exist_ok=True)
+
+# Define the file path
+file_path = os.path.join(data_dir, 'sample_data.csv')
+
+# Save the DataFrame to a CSV file, including column names
+df.to_csv(file_path, index=False)
+
+print(f"CSV file saved to {file_path}")
