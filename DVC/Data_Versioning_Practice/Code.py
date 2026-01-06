c436cd5ff5fae15dd57e03d37f87336e8c47d371
Yashraj Sharma <yashrajsharma12012002@gmail.com>
2026-01-06 14:21:02 +0530

    Third-version-data

--- DVC/Data_Versioning_Practice/Code.py
+++ DVC/Data_Versioning_Practice/Code.py
@@ -14,9 +14,9 @@ df = pd.DataFrame(data)
 new_row_loc = {'Name': 'D1', 'Age': 30, 'City': 'City1'}
 df.loc[len(df.index)] = new_row_loc
 
-# Adding new row to df for V3
-# new_row_loc = {'Name': 'V1', 'Age': 32, 'City': 'City1'}
-# df.loc[len(df.index)] = new_row_loc
+#Adding new row to df for V3
+new_row_loc = {'Name': 'V1', 'Age': 32, 'City': 'City1'}
+df.loc[len(df.index)] = new_row_loc
 
 # Ensure the 'data' directory exists at the root level
 data_dir = 'data'
