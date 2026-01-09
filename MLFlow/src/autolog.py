bb7c3958c7783241703abc26ca83493fea824604
Yashraj Sharma <yashrajsharma12012002@gmail.com>
2026-01-09 16:19:46 +0530

    "Autologging_in_MLFlow"

---
+++ MLFlow/src/autolog.py
@@ -0,0 +1,69 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,confusion_matrix
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load dataset
+data = load_wine()
+
+X = pd.DataFrame(data.data, columns=data.feature_names)
+y = pd.Series(data.target)
+
+# Split dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    test_size=0.2,
+                                                    random_state=42)
+
+#Define Rf model
+max_depth = 10
+n_estimators = 10
+
+mlflow.set_tracking_uri("http://localhost:5000")
+mlflow.set_experiment("Wine_Quality_Classification")
+mlflow.autolog()
+
+# Start MLflow experiment
+with mlflow.start_run():
+    # Intialize and train the model:
+    rf=RandomForestClassifier(max_depth=max_depth,
+                              n_estimators=n_estimators)
+    rf.fit(X_train,
+           y_train)
+
+    # Make predictions
+    y_pred=rf.predict(X_test)
+
+    # Evaluate the model
+    accuracy = accuracy_score(y_test,
+                            y_pred)
+    cm = confusion_matrix(y_test,
+                          y_pred)
+
+    #log Confusion matrix as image
+    plt.figure(figsize=(8,6))
+    sns.heatmap(cm,
+                annot=True,
+                fmt='d',
+                cmap='Blues',
+                xticklabels=data.target_names,
+                yticklabels=data.target_names)
+    plt.title('Confusion Matrix')
+    plt.xlabel('Predicted Label')
+    plt.ylabel('True Label')
+    plt.savefig("confusion_matrix.png")
+    plt.close()
+
+
+    # Log parameters and metrics to MLflow
+    mlflow.log_artifact(__file__)
+    mlflow.set_tag("author", "Yashraj Sharma")
+    mlflow.set_tag("project", "Wine Quality Classification")
+
+    print(f"Model accuracy:{accuracy}")
